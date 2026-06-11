6d6bfc60f53cf281c458c8b6d8171e21649ccfd4
Jake Goritski <jgoritski@headspin.io>
2026-06-10 18:02:30 -0700

    flush writes after writing data and newline

--- 1-prime-time/main.go
+++ 1-prime-time/main.go
@@ -37,36 +37,41 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
 	for {
-		message, err := rw.ReadBytes('\n')
+		message, err := reader.ReadBytes('\n')
 		if message != nil {
 			log.Printf("Received '%s'", strings.TrimSpace(string(message)))
 		}
 		if err != nil {
 			log.Printf("Failed to read request: %v", err)
-			rw.WriteString(`{"error": "Malformed request"}`)
+			writer.WriteString(`{"error": "Malformed request"}`)
+			writer.WriteString("\n")
+			writer.Flush()
 			return
 		}
 		request := testRequest{}
 		err = json.Unmarshal(message, &request)
 		if err != nil {
 			log.Printf("Malformed request: %v", err)
-			rw.WriteString(`{"error": "Malformed request"}`)
+			writer.WriteString(`{"error": "Malformed request"}`)
+			writer.WriteString("\n")
+			writer.Flush()
 			return
 		}
 		if request.Method != "isPrime" {
 			log.Printf("Invalid method: %s", request.Method)
-			rw.WriteString(`{"error": "Malformed request"}`)
+			writer.WriteString(`{"error": "Malformed request"}`)
+			writer.WriteString("\n")
+			writer.Flush()
 			return
 		}
 		prime := big.NewInt(int64(request.Number)).ProbablyPrime(0)
 		response, err := json.Marshal(testResponse{Method: "isPrime", Prime: prime})
 		log.Printf("Result: '%s'", response)
-		_, err = rw.WriteString(string(response))
-		if err != nil {
-			log.Panic(err)
-			return
-		}
+		writer.WriteString(string(response))
+		writer.WriteString("\n")
+		writer.Flush()
 	}
 }
